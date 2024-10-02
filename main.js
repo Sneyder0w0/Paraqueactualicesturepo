082321ee67d9ab938dc38853b2e4d5bdad0f9724
Sneyder Orellana <forsakenpenumbra@gmail.com>
2024-10-01 21:57:38 -0600

    pato

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-function click(){
-    
+class Contador{
+    constructor(){
+        if(Contador.instance){
+            return Contador.instance
+        }
+        this.click=0;
+        Contador.instance = this; 
+    }
+    añadirClick(){
+        this.click++
+    }
+    contarClick(){
+        return this.click;
+    }
+}
+
+function actualizarClick(){
+    const contador = new Contador();
+    contador.añadirClick();
+    document.getElementById("contador").textContent= contador.contarClick();
 }
